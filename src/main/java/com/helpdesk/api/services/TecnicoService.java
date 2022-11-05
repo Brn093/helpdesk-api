06fb58b925c508da18a3fb8275ffe7467e276ded
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-05 13:08:52 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/helpdesk/api/services/TecnicoService.java
+++ src/main/java/com/helpdesk/api/services/TecnicoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.helpdesk.api.exception.ObjectNotFoundException;
 import com.helpdesk.api.model.Tecnico;
 import com.helpdesk.api.repository.TecnicoRepository;
 
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
