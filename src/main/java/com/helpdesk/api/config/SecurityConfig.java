944a0f9d66abb59094a8dbda8c9932c255ff984b
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-15 14:51:05 -0300

    Inserindo a Autorização com JWT nas classes

--- src/main/java/com/helpdesk/api/config/SecurityConfig.java
+++ src/main/java/com/helpdesk/api/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.helpdesk.api.security.JWTAuthenticationFilter;
+import com.helpdesk.api.security.JWTAuthorizationFilter;
 import com.helpdesk.api.security.JWTUtil;
 
 @EnableWebSecurity
@@ -41,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
